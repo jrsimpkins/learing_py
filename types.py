c2898801e6391064866fa985e224c5e46878d372
jrsimpkins <jason@jasonsimpkins.com>
2020-02-11 18:11:41 -0700

    learning difrent types of srtings.

---
+++ types.py
@@ -0,0 +1,34 @@
+zero_chars = ''
+
+name = 'Matt'
+
+print(name)
+
+print(zero_chars)
+
+with_quote = "i ain't gonna"
+
+print(with_quote)
+
+print(zero_chars)
+
+longer = """this string has
+multiple lines
+in it"""
+
+print(longer)
+
+print(zero_chars)
+
+latin = '''Lorum ipsum
+dolor'''
+
+print(latin)
+
+print(zero_chars)
+
+escaped = 'i ain\'t gonna'
+
+print(escaped)
+
+print(zero_chars)
